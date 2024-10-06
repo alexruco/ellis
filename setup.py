abda2fa69c489047d20446d86e177b7265b13b6e
Alex Ruco <alex@ruco.pt>
2024-10-06 08:14:36 +0100

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ellis',  # This will be the name of your package
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=[
         # Add any external dependencies here
