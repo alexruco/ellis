4a13a11755355dec3d1d61dc41c7b492c4e59161
Alex Ruco <alex@ruco.pt>
2024-10-07 11:53:14 +0100

    new function send message aded to init

--- ellis/__init__.py
+++ ellis/__init__.py
@@ -1,3 +1,3 @@
 # ellis/__init__.py
-from ellis.main import get_history, get_new_messages
+from ellis.main import get_history, get_new_messages, send_message
 from ellis.db_connector import init_db, get_connection
