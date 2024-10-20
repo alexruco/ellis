62be5d847553c7c642fc73687becca3d604b47e8
Alex Ruco <alex@ruco.pt>
2024-10-20 23:19:00 +0100

    exatract emails adress on init

--- ellis/__init__.py
+++ ellis/__init__.py
@@ -1,3 +1,4 @@
 # ellis/__init__.py
 from ellis.main import get_history, get_new_messages, send_message
 from ellis.db_connector import init_db, get_connection
+from ellis.utils import extract_email_address
